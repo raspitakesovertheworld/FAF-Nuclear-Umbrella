49ef83eb4e15eb73a516979093bc8450fe78b965
Markus Bawidamann <markustheswiss@gmail.com>
2020-12-20 09:51:03 -0800

    Add timer code and update with polling

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import time
 import PySimpleGUI as sg
 import threading
 
-
+global start_time
+start_time = time.time()
 
 def counter_thread(window):
-    print(event, values)
+
+    start_time = time.time()
     scout = int(values["-scout_in-"])
     smd_start = int(values["-smd_start_in-"])
     smd_warning = int(values["-smd_warning_in-"])
@@ -27,8 +29,8 @@ if __name__ == '__main__':
 
     engine = pyttsx3.init()
     engine.setProperty('rate', 125)
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[0].id)
+    #voices = engine.getProperty('voices')
+    #engine.setProperty('voice', voices[0].id)
 
 
     sg.theme('BluePurple')
@@ -36,6 +38,7 @@ if __name__ == '__main__':
     layout = [[sg.Text('Scout warning after (mins):'), sg.Text(size=(4,1), key='-scout-')], [sg.Input(size=(4,1),key='-scout_in-',default_text="10")],
               [sg.Text('Start building SMD (mins):'), sg.Text(size=(4,1), key='-smd_start-')], [sg.Input(size=(4,1),key='-smd_start_in-',default_text="20")],
               [sg.Text('SMD be ready warning (mins):'), sg.Text(size=(4, 1), key='-smd_warning-')],[sg.Input(size=(4, 1), key='-smd_warning_in-',default_text="25")],
+              [sg.Text('Time elapsed:'), sg.Text(size=(4, 1), key='-timer-')],
               [sg.Button('Start'), sg.Button('Exit')]]
 
     sg.Input()
@@ -43,17 +46,18 @@ if __name__ == '__main__':
     window = sg.Window('FAF Nuclear Umbrella Clicker',layout, size=(400,300))
 
     while True:  # Event Loop
-        event, values = window.read()
-
-
+        event, values = window.read(timeout=10)
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
         if event == 'Start':
             # Update the "output" text element to be the value of "input" element
-            #window['-OUTPUT-'].update(values['-IN-'])
 
-            start_time =time.time()
-            threading.Thread(target=counter_thread, args=(window,), daemon=True).start()
-
+            counter_thread_instance = threading.Thread(target=counter_thread, args=(window,), daemon=True)
+            counter_thread_instance.start()
+            time.sleep(1)
+        time_elapsed = int(time.time() - start_time)
+        window['-timer-'].update('{:02d}:{:02d}.{:02d}'.format((time_elapsed // 100) // 60,
+                                                        (time_elapsed // 100) % 60,
+                                                        time_elapsed % 100))
 
     window.close()
