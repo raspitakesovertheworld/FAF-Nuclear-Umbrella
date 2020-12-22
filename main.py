0f603b50708299292f5dba9da4fce2af560c3a6a
Markus Bawidamann <markustheswiss@gmail.com>
2020-12-22 11:30:54 -0800

    First Beta Release V0.9mbeta

--- main.py
+++ main.py
@@ -9,28 +9,41 @@ start_time = time.time()
 def counter_thread(window):
 
     start_time = time.time()
-    scout = int(values["-scout_in-"])
-    smd_start = int(values["-smd_start_in-"])
-    smd_warning = int(values["-smd_warning_in-"])
+    scout_min = int(values["-scout_in-"])
+    scout_sec = scout_min * 60
+    smd_start_min = int(values["-smd_start_in-"])
+    smd_start_sec = smd_start_min * 60
+    smd_warning_min = int(values["-smd_warning_in-"])
+    smd_warning_sec = smd_warning_min * 60
     engine.say("Nuclear Umbrella started...")
     engine.runAndWait()
-    time.sleep(scout * 60)
-    print(time.time() - start_time)
-    engine.say(str(scout) + " minutes passed: Don't be blind, scout!")
+
+    #first wait: Scouting
+    time.sleep(scout_sec)
+    elapsed_time_min = int((time.time() - start_time) / 60)
+    engine.say(str(elapsed_time_min) + " minutes have passed: Don't be blind, make sure you scout!")
     engine.runAndWait()
-    time.sleep(smd_start * 60)
-    engine.say(str(smd_start) + " minutes elapsed, reminder: Start to build nuke defense!")
+
+    #second wait: start build SMD
+    elapsed_time_min = int((time.time() - start_time) / 60)
+    time.sleep((smd_start_min - elapsed_time_min)*60 )
+    elapsed_time_min = int((time.time()-start_time)/60)
+    engine.say(str(elapsed_time_min) + " minutes elapsed, reminder: Start building the nuclear missile defense!")
     engine.runAndWait()
-    time.sleep(smd_start * 60)
-    engine.say(str(smd_start) + " minutes elapsed, reminder: Start to build nuke defense!")
+
+    #third wait: SMD must be ready
+    time.sleep((smd_warning_min - elapsed_time_min)*60)
+    elapsed_time_min = int((time.time() - start_time) / 60)
+    engine.say(str(elapsed_time_min) + " minutes passed, WARNING: Must have SMD ready, with missile built! There is a high risk of rain!")
     engine.runAndWait()
 
 if __name__ == '__main__':
 
     engine = pyttsx3.init()
     engine.setProperty('rate', 125)
-    #voices = engine.getProperty('voices')
-    #engine.setProperty('voice', voices[0].id)
+    voices = engine.getProperty('voices')
+    print(voices)
+    engine.setProperty('voice', voices[2].id)
 
 
     sg.theme('BluePurple')
@@ -38,12 +51,12 @@ if __name__ == '__main__':
     layout = [[sg.Text('Scout warning after (mins):'), sg.Text(size=(4,1), key='-scout-')], [sg.Input(size=(4,1),key='-scout_in-',default_text="10")],
               [sg.Text('Start building SMD (mins):'), sg.Text(size=(4,1), key='-smd_start-')], [sg.Input(size=(4,1),key='-smd_start_in-',default_text="20")],
               [sg.Text('SMD be ready warning (mins):'), sg.Text(size=(4, 1), key='-smd_warning-')],[sg.Input(size=(4, 1), key='-smd_warning_in-',default_text="25")],
-              [sg.Text('Time elapsed:'), sg.Text(size=(4, 1), key='-timer-')],
+              [sg.Text('Time elapsed:'), sg.Text(size=(10, 1), key='-timer-')],
               [sg.Button('Start'), sg.Button('Exit')]]
 
     sg.Input()
 
-    window = sg.Window('FAF Nuclear Umbrella Clicker',layout, size=(400,300))
+    window = sg.Window('FAF Nuclear Umbrella Timer',layout, size=(400,300))
 
     while True:  # Event Loop
         event, values = window.read(timeout=10)
@@ -54,10 +67,8 @@ if __name__ == '__main__':
 
             counter_thread_instance = threading.Thread(target=counter_thread, args=(window,), daemon=True)
             counter_thread_instance.start()
-            time.sleep(1)
         time_elapsed = int(time.time() - start_time)
-        window['-timer-'].update('{:02d}:{:02d}.{:02d}'.format((time_elapsed // 100) // 60,
-                                                        (time_elapsed // 100) % 60,
-                                                        time_elapsed % 100))
+        window['-timer-'].update('{:02d}:{:02d}.{:02d}'.format((time_elapsed // 60) // 60, (time_elapsed // 60) % 60, time_elapsed % 60))
 
+        #window['-timer-'].update(str(time_elapsed / 60 /60) +":"+str(time_elapsed / 60)+":"+str(time_elapsed))
     window.close()
